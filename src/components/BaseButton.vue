0fa39d250e5282bfbf81ffa4261414aa169169bc
James Coliz <jcoliz@outlook.com>
2024-11-27 09:28:46 -0800

    feat: Add BaseButton component
    Signed-off-by: James Coliz <jcoliz@outlook.com>

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+/**
+ * Base Button: Standardized button styling
+ */
+defineProps<{
+  /**
+   * Visual styling to apply
+   *
+   * We can add more options to this list as long as bootstrap supports
+   * them. We want to keep it limited, though, so we keep a consistent
+   * look throughout.
+   *
+   * TODO: Is this list too many? Both `light` state and `outline-secondary`
+   * are used as tertiary visuals. Should reconsider this.
+   */
+  visual: "primary" | "secondary" | "danger" | "outline-secondary" | "light"
+}>()
+</script>
+<template>
+  <button
+    class="btn"
+    :class="`btn-${visual}`"
+  >
+    <slot />
+  </button>
+</template>
