bb88b2d6baeef04901eee7c2dc3cc380b0a4c838
James Coliz <jcoliz@outlook.com>
2024-11-27 10:20:03 -0800

    build: Builds library ok now

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "VueBaseButton",
+      fileName: "vue-base-button"
+    },
+    rollupOptions: {
+      external: ["vue", "bootstrap"],
+      output: {
+        globals: {
+          vue: "Vue"
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  }  
 })
